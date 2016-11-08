2fc191484c436fc6272959bd4d54fd531d563999
Josh Bodah <jb3689@yahoo.com>
2016-11-07 23:33:27 -0500

    add sliding windows

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   context: __dirname + "/app",
   entry: {
-    infiniteScroll: "./infiniteScroll/app"
+    infiniteScroll: "./infiniteScroll/app",
+    slidingWindows: "./slidingWindows/app"
   },
   output: {
     path: __dirname + "/dist",
